343c8829be2f3ad1c60ced229db8a22589ad0238
Jacinth <jacinthcedricbarral@gmail.com>
2024-02-06 22:11:18 +0800

    Java New Beginnings

---
+++ Java/Practice/src/expressions.java
@@ -0,0 +1,19 @@
+
+
+public class expressions{
+
+
+    public static void main(String[]args){
+
+        // Expressions = operands & operators
+        // Operands = values, variables, numbers, quantity
+        // Operators = + - * / %
+
+        int number =10;
+        number = number +1;
+
+        System.out.println(number);
+
+
+    }
+}
---
+++ Java/Practice/src/userInput.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class userInput {
+    
+    public static void main(String[] args){
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("How old are you? ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("What is you Favorite Dessert? ");
+        String favFood = scanner.nextLine();
+
+
+        System.out.println("Hello!, " +name);
+        System.out.println("You are " +age + " years old!");
+        System.out.println("Your Favorite Dessert is " +favFood);
+
+
+
+    }
+}

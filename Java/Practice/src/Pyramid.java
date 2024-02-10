4e8cc570b8adf8171ba37fc622294b65e54976de
Jacinth <jacinthcedricbarral@gmail.com>
2024-02-11 00:24:33 +0800

    Pyramid

---
+++ Java/Practice/src/Pyramid.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Pyramid{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        int rows = 7, cols = 5;
+        String symbol = "";
+        
+        
+        for(int i = 0; i < rows; i++)
+        {
+            
+            for(int s = 1; s <= rows - i; s++){
+                System.out.print(" ");
+            }
+            
+            for(int j = 1; j <= i * 2 - 1;  j++)
+            {
+                System.out.print("*");      
+                
+            }
+            System.out.println();
+
+        }        
+        
+        
+    }
+}

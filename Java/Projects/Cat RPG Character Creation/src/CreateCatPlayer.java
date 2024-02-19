ed9c6692f11c1ddd576b20953f0767f76d056a6e
Jacinth <jacinthcedricbarral@gmail.com>
2024-02-19 22:09:57 +0800

    Add Game Loop Where after a player selected and picked an option in the cat creation screen, it will loop back after

--- Java/Projects/Cat RPG Character Creation/src/CreateCatPlayer.java
+++ Java/Projects/Cat RPG Character Creation/src/CreateCatPlayer.java
@@ -1,17 +1,21 @@
-//import java.util.Arrays;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
 
 class CreateCatPlayer {
     Scanner in = new Scanner(System.in);
+    ArrayList<String> catValueList = new ArrayList<String>();
     static byte noOfLegs = 4;
     String[] genderArr;
     String[] colorArr;
     String[] patternArr;
     String[] ageArr;
     String[] eyeColorArr;
-    String catGender, catColor, catPattern, catAge, catEyeColor;
+    String[] userOptions;
+    String catGender, catColor, catPattern, catAge, catEyeColor, catName;
     byte playerSelect, response;
     boolean validInput;
 
@@ -24,7 +28,12 @@ class CreateCatPlayer {
         this.patternArr = new String[]{"Solid", "Tabby", "Tortoise", "Color-point", "Bi-colour", "Tricolour (Calico)", };
         this.ageArr = new String[]{"Teen", "Mid-teen", "Adult", "Mid-adult", "Old"};
         this.eyeColorArr = new String[]{"Hazel", "Brown", "Gold", "Green", "Blue"};
-
+        this.userOptions = new String[]{"Gender: None", "Eye-Colour: None ", "Color: None ","Pattern: None","Age: None", "Name: None"};
+    }
+    void setArrayList(){
+        for(byte i = 0; i <= userOptions.length; i++){
+            catValueList.set((i+1), userOptions[i]);
+        }
     }
     void displayArray(String[] array){
         for(byte i = 0; i < array.length; i++){
@@ -32,12 +41,28 @@ class CreateCatPlayer {
         }
 
     }
+
+    void determineTitle(){
+        String stringBuffer = "";
+        for(byte i = 0; i < userOptions.length; i++){
+            if(playerSelect == (i + 1)){
+                stringBuffer = userOptions[i];
+                stringBuffer = userOptions[i].replace(": None", "").trim().toUpperCase();
+                System.out.println("\n====================================================================");
+                System.out.println("--------------------------" + stringBuffer + "---------------------------");
+                System.out.println("====================================================================\n");
+            }
+        }
+//        playerSelect = 0;
+    }
+
     void playerSelect(String[] array){
         playerSelect = 0;
         response = 0;
         do {
 
             try{
+                determineTitle();
                 System.out.println("\nEnter #" + "1 to " + "#" + array.length + " to Select, 0 to Cancel\n");
                 System.out.print("---------> ::  ");
                 response = in.nextByte();
@@ -71,6 +96,69 @@ class CreateCatPlayer {
 
     }
 
+
+    void dispAndSelect(String[] array){
+
+        displayArray(array);
+        playerSelect(array);
+
+    }
+
+    void determineCatValue(String[] array, String catValue){
+        for(byte i = 0; i < array.length; i++){
+            if(playerSelect == (i + 1)){
+                catValue = array[i];
+                System.out.println("\n\n" + catValue);
+
+            }
+        }
+//        playerSelect = 0;
+    }
+    void gameLoop(){
+        for(byte i = 0; i < userOptions.length; i++){
+            // An attempt in looping when ever if you select and pick
+            // it will loop back to the main menu and loops again
+            // up until the user states if he/she is finish creating the character
+        }
+    }
+    void gameState(){
+        determineTitle();
+        switch(playerSelect){
+            case 1:
+
+                dispAndSelect(genderArr);
+                determineCatValue(genderArr, catGender);
+                displayArray(userOptions);
+
+                break;
+            case 2:
+                dispAndSelect(eyeColorArr);
+                determineCatValue(eyeColorArr, catEyeColor);
+                break;
+            case 3:
+                dispAndSelect(colorArr);
+                determineCatValue(colorArr, catColor);
+                break;
+            case 4:
+                dispAndSelect(patternArr);
+                determineCatValue(patternArr, catPattern);
+                break;
+            case 5:
+                dispAndSelect(ageArr);
+                determineCatValue(ageArr, catAge);
+                break;
+            case 6:
+
+                break;
+            case 0:
+                System.out.println("Bye!");
+
+                break;
+
+
+
+        }
+    }
 
 
 
--- Java/Projects/Cat RPG Character Creation/src/Main.java
+++ Java/Projects/Cat RPG Character Creation/src/Main.java
@@ -6,11 +6,49 @@ public class Main {
         System.out.println("\t\t\t\tRPG Cat Character Creation ver. 2");
         System.out.println("====================================================================\n");
 
-
-
-
+        System.out.println("\n====================================================================");
+        System.out.println("--------------------------CREATE YOUR CAT---------------------------");
+        System.out.println("====================================================================\n");
 
         CreateCatPlayer catOne = new CreateCatPlayer();
+
+        catOne.displayArray(catOne.userOptions);
+        catOne.playerSelect(catOne.userOptions);
+
+        catOne.gameState();
+
+/*        switch(catOne.playerSelect){
+            case 1:
+                catOne.dispAndSelect(catOne.genderArr);
+                catOne.determineCatValue(catOne.genderArr, catOne.catGender);
+                break;
+            case 2:
+                catOne.dispAndSelect(catOne.eyeColorArr);
+                catOne.determineCatValue(catOne.eyeColorArr, catOne.catEyeColor);
+                break;
+            case 3:
+                catOne.dispAndSelect(catOne.colorArr);
+                catOne.determineCatValue(catOne.colorArr, catOne.catColor);
+                break;
+            case 4:
+                catOne.dispAndSelect(catOne.patternArr);
+                catOne.determineCatValue(catOne.patternArr, catOne.catPattern);
+                break;
+            case 5:
+                catOne.dispAndSelect(catOne.ageArr);
+                catOne.determineCatValue(catOne.ageArr, catOne.catAge);
+                break;
+            case 6:
+
+                break;
+            case 0:
+                System.out.println("Bye!");
+
+                break;
+
+
+
+        }*/
 
 
 //
@@ -18,4 +56,5 @@ public class Main {
 //        catOne.playerSelect(catOne.patternArr);
 
     }
+
 }

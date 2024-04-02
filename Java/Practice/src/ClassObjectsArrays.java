0f0b608d21c14ec20d2b9600edbb52d036f70ae3
Jacinth <jacinthcedricbarral@gmail.com>
2024-04-02 20:37:02 +0800

    Activity nine for comprog

---
+++ Java/Practice/src/ClassObjectsArrays.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ClassObjectsArrays {
+    public static void main(String[] args) {
+        
+        Student[] students;
+
+        for(byte i = 0; i < 3; i++){
+            
+        }
+
+
+
+    }   
+}
+
+
+
+
+class Student{
+
+    String studentName;
+    int[] studentGrades;
+
+    Student(){
+
+
+    }
+}

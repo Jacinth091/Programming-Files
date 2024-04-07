acb93a53caa79a0a44b7f35db578a7dfc938ba2f
Jacinth <jacinthcedricbarral@gmail.com>
2024-04-07 14:44:20 +0800

    Added randomness to the Temp, and Humidty Will revise the condtions for different weather conditions

--- C/Practice/WeatherForecastingInC.c
+++ C/Practice/WeatherForecastingInC.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <strings.h>
 
 
 typedef enum {
-
+    // Unknown,
     Sunny =1,
     Cloudy,
     Rainy,
     Snowy,
     Windy,
     Stormy,
+    Clear,
+    Extreme,
 
 
 }WeatherCondi;
@@ -19,43 +22,85 @@ typedef struct {
 
     int weather;
     char cityName[50];
-    float cityTemp, humidity;
+    int humid;
+    float fahren, celsi;
+
 
 
 }City;
 
+void cityDataList(City *cityData, int length, char *city[]);
+
 int main(){
 
-    srand(time(0));
+    char *city[] = {"New York", "Los Angeles", "Chicago", "Houston", "Phoenix", "Philadelphia", 
+                    "San Antonio", "San Diego", "Dallas", "San Jose"};
+    const int numToForecast =10;
+    City cityData[numToForecast];
+    
+    cityDataList(cityData, numToForecast, city);
+    
+    printf("\n\n"); 
+    printf("%5s  \t %5s  \t %5s \t %5s\n", "Fahrenheit", "Celsius", "Humidty", "Weather"); 
+    for(int i =0; i < numToForecast; i++){
+        printf(" %5.1f F \t %5.1f C \t %5.d %% \t %5d\n", cityData[i].fahren, cityData[i].celsi,cityData[i].humid, cityData[i].weather);
 
-    WeatherCondi weather = (rand() % 6) + 1;
+    }
+    
 
-    City cityOne;
-    cityOne.weather = weather;
+    return 0;
+}
 
-    switch (cityOne.weather) {
-        case Sunny:
-            printf("It's Currently, Sunny\n");
-            break;
-        case Cloudy:
-            printf("It's Currently, Cloudy\n");
-            break;
-        case Rainy:
-            printf("It's Currently, Rainy\n");
-            break;
-        case Snowy:
-            printf("It's Currently, Snowy\n");
-            break;
-        case Windy:
-            printf("It's Currently, Windy\n");
-            break;
-        case Stormy:
-            printf("It's Currently, Stormy\n");
-            break;
-        default:
-            printf("Unknown weather condition\n");
+
+
+void cityDataList(City *cityData, int length, char *city[]){
+
+    int rando =0, counter =10;
+    int minTemp = -200, maxTemp = 1200;
+    WeatherCondi weather;
+    srand(time(NULL));
+
+    for(int i = 0; i < length; i++){
+
+        strcpy(cityData[i].cityName, city[i]);
+        cityData[i].fahren =(float) (rand() % (maxTemp - minTemp + 1) + minTemp) / 10.0;
+        cityData[i].celsi = ((cityData[i].fahren - 32) * 5) / 9;
+        cityData[i].humid = rand() % (100 - 10 + 1) + 10;
+        cityData[i].weather = 0;
+
+        // Revise the Conditions for the Weather Conditions 
+
+        // // Sunny Conditions
+        // if((cityData[i].fahren > 70) && (cityData[i].humid >= 30 && cityData[i].humid <= 60)){
+        //     weather = Sunny;
+        // }// Cloudy / Partly Cloudy
+        // else if((cityData[i].fahren >= 50 && cityData[i].fahren <= 80) && (cityData[i].humid >= 50 && cityData[i].humid <= 80)){
+        //     weather = Cloudy;
+        // }// Rainy
+        // else if((cityData[i].fahren >= 45 && cityData[i].fahren <= 75) && (cityData[i].humid >= 60 && cityData[i].humid <= 100)){
+        //     weather = Rainy;
+        // }// Snowy
+        // else if((cityData[i].fahren >= 20 && cityData[i].fahren <= -20) && (cityData[i].humid >= 20 && cityData[i].humid <= 80)){
+        //     weather = Snowy;
+        // }// Windy
+        // else if((cityData[i].fahren > 50) && (cityData[i].humid >= 30 && cityData[i].humid <= 60)){
+        //     weather = Windy;
+        // }// Stormy
+        // else if((cityData[i].fahren > 60) && (cityData[i].humid >= 70 && cityData[i].humid <= 100)){
+        //     weather = Stormy;
+
+        // }
+        // else if((cityData[i].fahren >= 50 && cityData[i].fahren <= 80) && (cityData[i].humid >= 30 && cityData[i].humid <= 60)){
+        //     weather = Clear;
+        // }
+        // else if((cityData[i].fahren > 100)){
+        //     weather = Extreme;
+        // }
+        // cityData[i].weather = weather;
+
+
     }
 
 
-    return 0;
+
 }
--- C/Practice/tempCodeRunnerFile.c
+++ C/Practice/tempCodeRunnerFile.c
@@ -1,6 +1,27 @@
+    // Sunny Conditions
+        if((cityData[i].fahren > 70) && (cityData[i].humid >= 30 && cityData[i].humid <= 60)){
+            weather = Sunny;
+        }// Cloudy / Partly Cloudy
+        else if((cityData[i].fahren >= 50 && cityData[i].fahren <= 80) && (cityData[i].humid >= 50 && cityData[i].humid <= 80)){
+            weather = Cloudy;
+        }// Rainy
+        else if((cityData[i].fahren >= 45 && cityData[i].fahren <= 75) && (cityData[i].humid >= 60 && cityData[i].humid <= 100)){
+            weather = Rainy;
+        }// Snowy
+        else if((cityData[i].fahren >= 20 && cityData[i].fahren <= -20) && (cityData[i].humid >= 20 && cityData[i].humid <= 80)){
+            weather = Snowy;
+        }// Windy
+        else if((cityData[i].fahren > 50) && (cityData[i].humid >= 30 && cityData[i].humid <= 60)){
+            weather = Windy;
+        }// Stormy
+        else if((cityData[i].fahren > 60) && (cityData[i].humid >= 70 && cityData[i].humid <= 100)){
+            weather = Stormy;
 
-    // printf("\nEnter elements in array: \n");
-    // for(int i = 0; i < sizeof(arrOfNumbers)/sizeof(arrOfNumbers[0]); i++){
-    //     scanf("%d", &userIn);
-    //     arrOfNumbers[i] = userIn;
-    // } 
+        }
+        else if((cityData[i].fahren >= 50 && cityData[i].fahren <= 80) && (cityData[i].humid >= 30 && cityData[i].humid <= 60)){
+            weather = Clear;
+        }
+        else if((cityData[i].fahren > 100)){
+            weather = Extreme;
+        }
+        cityData[i].weather = weather;

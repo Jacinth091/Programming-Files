1cb6da7486879013f74467ea2a216878a08c403b
Jacinth <jacinthcedricbarral@gmail.com>
2024-04-07 22:50:56 +0800

    First C# file

---
+++ C#/Practice/FirstC#File/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstC_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Data Types in C#\n\n");
+
+            // Data Types in C#
+
+            // int = Integers
+            int age = 23;
+            Console.WriteLine(age);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}

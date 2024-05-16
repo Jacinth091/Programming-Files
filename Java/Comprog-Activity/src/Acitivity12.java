13049f61eb08181cf3e906f43cd80d5ed01c5be1
Jacinth091 <144292495+Jacinth091@users.noreply.github.com>
2024-05-16 18:33:33 +0800

    Activity 12 constructors
    Final Activity for Comprog 12

---
+++ Java/Comprog-Activity/src/Acitivity12.java
@@ -0,0 +1,120 @@
+/*
+ PARTIAL
+ Name: Barral, Jacinth Cedric C.
+ Date: May 16, 2024
+ Description: Using the object you have selected on our Prelim Lab.Act.#02 on Classes and Objects,
+                create a class of that object and implement a constructor (not less than 2 constructors).
+            Use the constructor to initialize the properties of the class (instance variables). 
+            Set the class properties (instance variables) to privateCreate getters and setters for each field.
+            Write a main method to instantiate the selected object,
+            set values using the constructor, modify values using setters,
+                and print out the values using getters.
+ */
+
+
+class Acitivity12 {
+    public static void main(String[] args){
+        int noOfSandwiches = 5;
+        Sandwich[] sandwiches = new Sandwich[noOfSandwiches];
+
+        
+
+
+
+
+
+
+
+
+
+        
+
+    }
+    
+}
+class Storage{
+
+    String[] breads = {"White", "Whole Wheat", "Rye", "Sourdough", "Multigrain", "Ciabatta", "Baguette"};
+    String[] meats = {"Turkey", "Ham", "Chicken", "Roast Beef", "Salami", "Tuna", "Bacon"};
+    String[] toppings = {"Lettuce", "Tomato", "Onion", "Cucumber", "Olives", "Peppers", "Cheese"};
+    String[] sauces = {"Mayo", "Mustard", "Honey Mustard", "Ranch", "Chipotle", "BBQ", "Hummus"};
+
+    String[] menu = {"Create Your Own Sandwich", "View Sandwiches (Pre-Created)"};
+
+    public void displayArray(String[] array){
+        for(int i =0; i < array.length; i++){
+            System.out.printf("%s", array[i]);
+        }
+
+    }
+}
+
+
+class Sandwich{
+    private String bread;
+    private String meat;
+    private String toppings;
+    private String sauce;
+    private int layers;
+    private int noOfToppings;
+
+    Sandwich(){
+        
+    }
+    Sandwich(String bread, String meat, String toppings, String sauce){
+        this.setBread(bread);
+        this.setMeat(meat);
+        this.setToppings(toppings);
+        this.setSauce(sauce);
+    }
+
+
+
+    public void setBread(String bread){
+        this.bread = bread;
+    }
+    public String getBread(){
+        return this.bread;
+    }
+
+    public void setMeat(String meat){
+        this.meat = meat;
+    }
+    public String getMeat(){
+        return this.meat;
+    }
+
+
+    public void setToppings(String toppings){
+        this.toppings = toppings;
+    }
+    public String getToppings(){
+        return this.bread;
+    }
+
+
+    public void setSauce(String sauce){
+        this.sauce = sauce;
+    }
+    public String getSauce(){
+        return this.sauce;
+    }
+
+
+    public void setLayers(int noOfLayers){
+        this.layers = noOfLayers;
+    }
+    public int getLayers(){
+        return this.layers;
+    }
+
+
+    public void setNoOfTopp(int noOfToppings){
+        this.noOfToppings = noOfToppings;
+    }
+    public int getNoOfTopp(){
+        return this.noOfToppings;
+    }
+
+}
+

95a709356509c6293758e186aa2f68d1305086ae
Jacinth <jacinthcedricbarral@gmail.com>
2024-08-19 20:48:30 +0800

    Modified the Act Arrays for OOPROG Activity

--- Java/OOPROG - JAVA/Practice/ActArray.java
+++ Java/OOPROG - JAVA/Practice/ActArray.java
@@ -14,35 +14,20 @@ class Main
 
 		int[] numArr = new int[5];
 		double ave =0;
+
 		int max = 0, min =0, num =0;
+
+		int maxGrade =100, minGrade =70;
+
 		Scanner in = new Scanner(System.in);
 
-		System.out.println("This program will accept 5 grades, and will print the minumum, \nmaximum and the average grade.");
+		System.out.println("This program will accept 5 grades, and will print the minimum, \nmaximum and the average grade.");
 
-		for(int i =0; i< numArr.length; i++) {
-			System.out.print("Enter grade #" + (i+1) + ": ");
-		    
-		    // num = in.nextInt(); 
-		    if(in.hasNextInt()){
-            num = in.nextInt();
-             if(num <= 70 || num >= 100){
-     			   System.out.println("Enter grades between 70 to 100 only.");
-     			   i--;
-     			   continue;
-    			}
-    			else{
-    			    numArr[i] = num;
-    			}
-		    }
-		    else{
-		        System.out.println("Wrong input, only whole numbers!!");
-		        in.next();
-		        i--;
-		    }
+		numArr = getGrades(numArr.length, minGrade, maxGrade, in);
 
-           
-		}
-		// get Mininmum value
+		in.close();
+
+		// get Minimum value
 		min = getMin(numArr);
 		
 		// get Maximum Value
@@ -51,15 +36,62 @@ class Main
 		// get Average Grade
 		ave = getAverage(numArr);
 		
-		
+
+		displayResult(max, min, ave);
+
+	}
+	public static int checkNum(Scanner in){
+		int tempNum;
+
+		if(in.hasNextInt()){
+			tempNum = in.nextInt();
+
+		}
+		else{
+			in.next();
+			return -100;
+		}
+		return tempNum;
+	}
+
+
+	public static int[] getGrades(int size,int minGrade, int maxGrade, Scanner in){
+		int[] tempArr = new int[size];
+		int tempNum;
+
+		for(int i= 0; i< size; i++){
+			System.out.print("Enter grade #" + (i+1) + ": ");
+
+			tempNum = checkNum(in);
+
+			if(tempNum == -100){
+				System.out.println("Invalid Input, try again!");
+				i--;
+            }
+			else if(tempNum < minGrade || tempNum > maxGrade){
+				System.out.println("Enter grades between 70 to 100 only.");
+				i--;
+            }
+			else{
+				tempArr[i] = tempNum;
+			}
+
+
+		}
+
+		return tempArr;
+	}
+
+
+
+	public static void displayResult(int max, int min, double ave){
 		System.out.printf("Min: %d \n", min);
 		System.out.printf("Max: %d \n", max);
 		System.out.printf("Average: %.2f\n", ave);
-
 	}
 	
 
-	public static double getAverage(int numArr[]) {
+	public static double getAverage(int[] numArr) {
         int total =0;
         
         for(int i =0; i< numArr.length; i++){

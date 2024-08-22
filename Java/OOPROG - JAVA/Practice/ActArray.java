580fbf817290b851d0862b97800b6155d45f674f
Jacinth <jacinthcedricbarral@gmail.com>
2024-08-22 22:04:01 +0800

    Modified the Act Arrays for OOPROG Activity

--- Java/OOPROG - JAVA/Practice/ActArray.java
+++ Java/OOPROG - JAVA/Practice/ActArray.java
@@ -15,31 +15,88 @@ class Main
 		int[] numArr = new int[5];
 		double ave =0;
 
-		int max = 0, min =0, num =0;
+		int max = 0,
+				min =0,
+				num =0;
 
-		int maxGrade =100, minGrade =70;
+		final int maxGrade =100,
+				minGrade =70;
+
 
 		Scanner in = new Scanner(System.in);
 
 		System.out.println("This program will accept 5 grades, and will print the minimum, \nmaximum and the average grade.");
 
-		numArr = getGrades(numArr.length, minGrade, maxGrade, in);
+		mainLoop(numArr, max, min, num, ave, minGrade, maxGrade, in);
+		// Main Logic Loop
 
 		in.close();
+	}
 
-		// get Minimum value
-		min = getMin(numArr);
-		
-		// get Maximum Value
-		max = getMax(numArr);
-		
-		// get Average Grade
-		ave = getAverage(numArr);
-		
+	public static void mainLoop(int[] numArr, int max, int min, int num, double ave, int minGrade, int maxGrade, Scanner in){
 
-		displayResult(max, min, ave);
+		boolean exitLoop = false;
+		int count =0;
+		do{
+			System.out.println("-------------------------------------------------------");
+
+			System.out.printf("\nYou have looped --%d--!\n", count++ );
+
+			System.out.println("Student's Grades\n");
+
+			numArr = getGrades(numArr.length, minGrade, maxGrade, in);
+
+			// get Minimum value
+			min = getMin(numArr);
+
+			// get Maximum Value
+			max = getMax(numArr);
+
+			// get Average Grade
+			ave = getAverage(numArr);
+
+
+			displayResult(max, min, ave);
+
+			exitLoop = checkYesOrNo(in);
+
+
+		}while(!exitLoop);
+
+		System.out.println("\n-------------------------------------------------------\n");
+
+		System.out.println("Thank you, come again :D");
+
+		System.out.println("\n-------------------------------------------------------");
+	}
+
+
+	public static boolean checkYesOrNo(Scanner in){
+		boolean value = false;
+		do{
+			char input;
+			System.out.println("\"Y\" to start again, \"N\" to exit the program.");
+			System.out.print("Your choice: ");
+
+			input = in.next().toUpperCase().charAt(0);
+
+			if(input == 'Y' || input == 'N'){
+				if( input == 'N'){
+
+					value = true;
+				}
+				break;
+			}
+			else{
+				System.out.println("Input only Y and N to function, try again.");
+			}
+
+		}while(true);
+
+		return value;
 
 	}
+
 	public static int checkNum(Scanner in){
 		int tempNum;
 
@@ -85,9 +142,11 @@ class Main
 
 
 	public static void displayResult(int max, int min, double ave){
+		System.out.println("-------------------------------------------------------");
 		System.out.printf("Min: %d \n", min);
 		System.out.printf("Max: %d \n", max);
 		System.out.printf("Average: %.2f\n", ave);
+		System.out.println("-------------------------------------------------------");
 	}
 	
 

2798372b06a66ec1abf71f142f2dd83cb839556d
Jacinth <jacinthcedricbarral@gmail.com>
2024-09-21 11:52:59 +0800

    OOP ACT FOUR - 2D Array for Student Grades The activity is about asking input and sorting the input and display the sorted output wth the computed average

--- Java/OOPROG - JAVA/Practice/ActFourOOP.java
+++ Java/OOPROG - JAVA/Practice/ActFourOOP.java
@@ -32,64 +32,39 @@ class ActFourOOP{
     
     final static int minGrade = 70;
     final static int maxGrade = 100;
+    static int numOfStud = 3, numOfGrd = 5;
     
     public static void main(String[] args){
         
         Scanner in = new Scanner(System.in);
-        int[][] numArr = new int[3][3];
-        
+        int[][] numArr = new int[numOfStud][numOfGrd];
         int[][] arr= new int[][] {
             {90, 79, 89, 70, 78, 100, 81},
             {70, 81, 91, 85, 80, 91, 78},
-            {78, 96, 87, 90, 84, 80, 91},
+            {99, 96, 87, 90, 84, 80, 91},
         };
-        
-        
-        // getStudentGrades(in, numArr);
-            
-        dispArray(arr);
-        
-        
-        for(int k = 0; k < arr.length; k++ ){
-            // arr[i] = sortingAlgo(arr[i]);
-                        
-            int minIndex;
-            int[] tempAr = arr[k];
-            // int stud = studGrades.length, grades = studGrades[0].length;
-            // int[] temp = new int[grades];
-            int n = tempAr.length;
-            
-            for(int  i=0; i < n -1; i++){
-                minIndex =i;
-                for(int j = i +1; j < n; j++){
-                    if(tempAr[j] < tempAr[minIndex]){
-                        minIndex =j;
-                    }
-    
-                }
-                if(minIndex != i){
-                    int temp = tempAr[minIndex];
-                    tempAr[minIndex] = tempAr[i];
-                    tempAr[i] = temp;
-                }
-    
-    
-    
-            }
-            
-        }
+
+
+//        sortingAlgo(arr);
+
+//        System.out.println();
+
+//        dispArray(arr);
+
+
+        getStudentGrades(in, numArr);
+        sortingAlgo(numArr);
         System.out.println();
-        
-        dispArray(arr);
+        dispArray(numArr);
         
     }
     
-    public static int[][] getStudentGrades(Scanner in, int[][] numArr){
+    public static void getStudentGrades(Scanner in, int[][] numArr){
         
         for(int i = 0; i < numArr.length; i++){
             boolean isValid = false;
             int num =0;
-            System.out.println("Enter grades for Student 1 (70 - 100): ");
+            System.out.printf("Enter grades for Student %d (%d - %d)\n\n", (i+1), minGrade, maxGrade);
             for(int j = 0; j < numArr[i].length; j++){
                 System.out.printf("Student's Grade %d: ", j+1);
                 
@@ -106,62 +81,50 @@ class ActFourOOP{
                 }
                 
             }
+            System.out.println();
         }
         
-        return numArr;
+//        return numArr;
     }
-    
-    // public static int[] sortStudentGrades(int[] studGrades){
-        
-    //     int minIndex, n = studGrades.length;
-    //     for(int i = 0; )
-        
-        
-    //     return studGrades;
-        
-        
-        
-        
-    // }
-    
-    public static int[] sortingAlgo(int[] studGrades){
-   
-        
-        int minIndex;
-        // int stud = studGrades.length, grades = studGrades[0].length;
-        // int[] temp = new int[grades];
-        int n = studGrades.length;
-        
-        for(int  i=0; i < n -1; i++){
-            minIndex =i;
-            for(int j = i +1; j < n; j++){
-                if(studGrades[j] < studGrades[minIndex]){
-                    minIndex =j;
+    public static void sortingAlgo(int[][] studGrades){
+        for(int k = 0; k < studGrades.length; k++ ){
+
+            int minIndex;
+            int[] tempAr = studGrades[k];
+            int n = tempAr.length;
+
+            for(int  i=0; i < n -1; i++){
+                minIndex =i;
+                for(int j = i +1; j < n; j++){
+                    if(tempAr[j] < tempAr[minIndex]){
+                        minIndex =j;
+                    }
                 }
-
+                if(minIndex != i){
+                    int temp = tempAr[minIndex];
+                    tempAr[minIndex] = tempAr[i];
+                    tempAr[i] = temp;
+                }
             }
-            if(minIndex != i){
-                int temp = studGrades[minIndex];
-                studGrades[minIndex] = studGrades[i];
-                studGrades[i] = temp;
-            }
-
-
-
         }
-
-
-
-        return studGrades;
     }
 
-    
+    public static double getAverage(int[] grades){
+        int total =0;
+        for(int grade : grades){
+            total += grade;
+        }
+
+        return (double) total / grades.length;
+    }
     public static boolean checkInputRange(int num){
         
-        if(num >= minGrade && num <= maxGrade){
-            return true;
-        }
-        return false;
+//        if(num >= minGrade && num <= maxGrade){
+//            return true;
+//        }
+//        return false;
+
+        return num >= minGrade && num <= maxGrade;
         
     }
     
@@ -180,15 +143,17 @@ class ActFourOOP{
     public static void dispArray(int[][] numArr){
         System.out.print("");
         // String dispFormat = "";
-
+        int stud =1;
         for(int[] students : numArr ){
-        //  int j =0;
+            System.out.printf("Sorted Grades for Student %d:\n", stud);
             for(int studGrade : students){
                 // j++;
                 // String dispFormat = (j != numArr[j].length -1) ? ", " : " ";
                 System.out.print(studGrade + " ");
             }
+            System.out.printf("\nAverage grade for Student %d: %.2f\n", stud, getAverage(students));
             System.out.println();
+            stud++;
         }
         
         

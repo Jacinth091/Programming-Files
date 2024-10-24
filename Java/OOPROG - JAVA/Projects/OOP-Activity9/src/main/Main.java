c23d950db96d96aefef3e84287c28076f60b5d0f
Jacinth <jacinthcedricbarral@gmail.com>
2024-10-24 22:31:43 +0800

    [Jacinth - Ui change] Changed how the frame and the mainpanel works unfinished

--- Java/OOPROG - JAVA/Projects/OOP-Activity9/src/main/Main.java
+++ Java/OOPROG - JAVA/Projects/OOP-Activity9/src/main/Main.java
@@ -1,6 +1,7 @@
 package main;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 
@@ -8,33 +9,65 @@ public class Main {
 
 
     public static void main(String[ ]args){
-        JFrame app = new MainFrame();
+/*        JFrame app = new MainFrame();
         MainPanel mp = new MainPanel();
         JPanel panel = new JPanel();
 
         app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         app.setResizable(true);
         app.setTitle("Activity 9???");
-        app.setPreferredSize(new Dimension(mp.screenWidth+100,mp.screenHeight + (mp.screenHeight /2)));
-
 
         System.out.println(mp.screenHeight);
         System.out.println(mp.screenWidth);
+        app.setPreferredSize(new Dimension(mp.screenWidth + 100,mp.screenHeight + (mp.screenHeight/3)));
+
+
         panel.setPreferredSize(new Dimension(150,150));
-        panel.add(mp);
+        panel.add(mp, BorderLayout.CENTER);
 
-        app.add(panel);
+        app.add(panel, BorderLayout.CENTER);
+
+
+
+
         app.pack();
-
-
         app.setLocationRelativeTo(null);
-        app.setVisible(true);
+        app.setVisible(true);*/
 
 //        mp.startThread();
 
+        appStart();
 
     }
 
 
+    public static void appStart(){
+        Border greyBorder = BorderFactory.createLineBorder(Color.gray, 20);
+        JFrame app = new JFrame();
+//        app.setSize(width, height);
+        app.setTitle("SF Lab. Act. #09");
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app.setResizable(false);
+        app.setLayout(new GridLayout(1,1));
 
+
+
+
+        MainPanel mp = new MainPanel();
+
+        app.setPreferredSize(new Dimension(mp.screenWidth + 100,mp.screenHeight + (mp.screenHeight/3)));
+        JPanel container = new JPanel();
+        container.setBorder(greyBorder);
+
+
+        container.setLayout(new BorderLayout());
+        container.add(mp, BorderLayout.CENTER);
+
+        app.add(container);
+        app.pack();
+
+        app.setLocationRelativeTo(null);
+        app.setVisible(true);
+
+    }
 }

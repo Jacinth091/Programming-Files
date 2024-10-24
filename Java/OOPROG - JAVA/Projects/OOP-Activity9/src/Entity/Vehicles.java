81472cf867ca0899a1452e332ba4a87f00bb36c7
Jacinth <jacinthcedricbarral@gmail.com>
2024-10-24 15:27:57 +0800

    [Jacinth - Added Functionality] Added Vehicle Interface that asks for a destination parameter Added Jeepney, Motorcycle, and Helicopter with extending to Entity and implementing Vehicle

---
+++ Java/OOPROG - JAVA/Projects/OOP-Activity9/src/Entity/Vehicles.java
@@ -0,0 +1,39 @@
+package Entity;
+
+import Events.KeyInput;
+import Events.MouseInput;
+import main.MainPanel;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public abstract class Vehicles extends Entity{
+    protected String vehicleType;
+    protected BufferedImage vhSprite;
+    protected int vehicleSpeed;
+    protected int vehicleCapacity;
+
+    protected MouseInput mI;
+    protected KeyInput kI;
+    protected MainPanel mp;
+
+    public Vehicles(){}
+
+    public Vehicles(MainPanel mp, MouseInput mI, KeyInput kI){
+        this.mp = mp;
+        this.mI = mI;
+        this.kI = kI;
+
+
+    }
+
+
+    public abstract void setLocation(int x, int y);
+
+    public abstract void update();
+    public abstract void draw(Graphics2D g2);
+
+
+
+
+}

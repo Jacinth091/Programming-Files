43a5674465d97adb381f3ac8c8aea4eb27df73cb
Jacinth <jacinthcedricbarral@gmail.com>
2025-03-15 14:21:56 +0800

    feat: Implemented MySinglyLinkedList activity

--- Java/Acitivities-folder/SinglyLinkedList/MySinglyLinkedList.java
+++ Java/Acitivities-folder/SinglyLinkedList/MySinglyLinkedList.java
@@ -1,235 +1,185 @@
-public class MySinglyLinkedList 
-{
-   MyNode top;
-   int count;
-   
-   public MySinglyLinkedList()
-   {
+public class MySinglyLinkedList {
+
+  MyNode top;
+  int count;
+
+  public MySinglyLinkedList() {
       top = null;
       count = 0;
-   }
-   
-   public boolean isEmpty()
-   {
-      return top == null;
-   }
-   
-   public boolean isFull()
-   {
-      return false;
-   }
-   
-   public int getCount()
-   {
-      int count = 0;
-      if(!isEmpty())
-      {
-         MyNode tempTop = top;
-         while(tempTop != null)
-         {
-            tempTop = tempTop.next;
-            count = count + 1;
-         }        
-      }
-      return count;
-   }
-   
-   public boolean isLocationValid(int location)
-   {
-      return location >= 0 && location <= getCount() - 1;
-   }
-   
-   
-   public boolean add(Object o)
-   {
-      MyNode newNode = new MyNode();
-      newNode.item = o;
-      
-      if(isEmpty())
-      {
-         top = newNode;
-      }
-      else 
-      {
-         MyNode tempTop = top;
-         while(tempTop.next != null)
-         {
-            tempTop = tempTop.next;
-         }
-         tempTop.next = newNode;         
-      }
-      return true;
-      
-   }
-   
-   public boolean search(Object o)
-   {
-      if(!isEmpty())
-      {
-         MyNode tempTop = top;
-         while(tempTop != null)
-         {
-            if(tempTop.item.toString().equalsIgnoreCase(o.toString()))
-            {
-               return true;
-            }
-            tempTop = tempTop.next;
-         }
+  }
+
+  // bool Search(Object o)
+  public boolean Search(Object o) {
+      if (!isEmpty()) {
+          MyNode myCurrentTempTop = top;
+          while (myCurrentTempTop != null) {
+              if (myCurrentTempTop.item.toString().equalsIgnoreCase(o.toString())) {
+                  return true;
+              }
+              myCurrentTempTop = myCurrentTempTop.next;
+          }
       }
       return false;
-   }
-   
-   public int searchPart2(Object o)
-   {
-      if(!isEmpty())
-      {
-         MyNode tempTop = top;
-         int location = 0;
-         while(tempTop != null)
-         {
-            if(tempTop.item.toString().equalsIgnoreCase(o.toString()))
-            {
-               return location;
-            }
-            location = location + 1;
-            tempTop = tempTop.next;            
-         }
+  }
+
+  // int SearchPart2(Object o)
+  public int SearchTwo(Object o) {
+      if (!isEmpty()) {
+          MyNode myCurrentTempTop = top;
+          int index = 0;
+          while (myCurrentTempTop != null) {
+              if (myCurrentTempTop.item.toString().equalsIgnoreCase(o.toString())) {
+                  return index;
+              }
+              index = index + 1;
+              myCurrentTempTop = myCurrentTempTop.next;
+          }
       }
       return -1;
-   }
-   
-   
-   public boolean delete(Object o)
-   {
-      if(!isEmpty() && search(o))
-      {
-         //scenario 1: the list contains only 1 node, that is the top
-         //and that top is the node to be deleted
-         if(top.item.toString().equalsIgnoreCase(o.toString()) && top.next == null)
-         {
-            top = null;
-            return true;
-         }
-         //scenario 2: the list contains more than 1 nodes, and the node to be
-         //deleted is the top
-         else if(top.item.toString().equalsIgnoreCase(o.toString()) && top.next != null)
-         {
-            top = top.next;
-            return true;
-         }
-         else 
-         {
-            System.out.println("*");
-            //scenario 3: the list contains more that 1 nodes, and the node to be
-            //deleted is somewhere in the middle or the last node
-            MyNode tempCurrentTop = top;
-            MyNode tempPreviousTop = null;
-            
-            while(tempCurrentTop != null)
-            {
-               if(tempCurrentTop.item.toString().equalsIgnoreCase(o.toString()))
-               {
-                  break;
-               }
-               tempPreviousTop = tempCurrentTop;
-               tempCurrentTop = tempCurrentTop.next;
-            }
-            
-            if(tempCurrentTop != null)
-            {
-               tempPreviousTop.next = tempCurrentTop.next;               
-               return true;
-            }                        
-         }
+  }
+
+  // bool Delete(Object o)
+  public boolean Delete(Object o) {
+      if (!isEmpty() && Search(o)) {
+          // Scenario 1: If the list only contains One item and it is the one to be deleted
+          if (top.item.toString().equalsIgnoreCase(o.toString()) && top.next == null) {
+              top = null;
+              return true;
+          }
+          // Scenario 2: If the List only contains 2 items and The item to be deleted is the top
+          else if (top.item.toString().equalsIgnoreCase(o.toString()) && top.next != null) {
+              top = top.next;
+              return true;
+          }
+          // Scenario 3: If the item to be deleted is in between the list or at the last
+          else {
+              MyNode myCurrentTop = top;
+              MyNode myPreviousTop = null;
+              while (myCurrentTop != null && !myCurrentTop.item.toString().equalsIgnoreCase(o.toString())) {
+                  myPreviousTop = myCurrentTop;
+                  myCurrentTop = myCurrentTop.next;
+              }
+              if (myCurrentTop != null) {
+                  myPreviousTop.next = myCurrentTop.next;
+                  return true;
+              }
+          }
       }
       return false;
-   }
-   
-   public boolean deleteByLocation(int location)
-   {
-      if(!isEmpty() && isLocationValid(location))
-      {
-         //scenario 1: if the location is 0, it means that it is the top
-         //node, therefore, set the top to its top next
-         if(location == 0)
-         {
-            top = top.next;
-            return true;
-         }
-         //scenario 2: if the node to be deleted is in the middle of the list
-         else 
-         {
-            int index = 0;
-            MyNode tempCurrentTop = top;
-            MyNode tempPreviousTop = null;
-            while(index < location)
-            {
-               tempPreviousTop = tempCurrentTop;
-               tempCurrentTop = tempCurrentTop.next;
-               index = index + 1;
-            }
-            
-            tempPreviousTop.next = tempCurrentTop.next;
-            return true;
-         }
+  }
+
+  // bool DeleteByLocation(int location)
+  public boolean DeleteByLocation(int location) {
+      if (!isEmpty() && isLocationValid(location)) {
+          // Scenario 1: If the list only contains One item and it is the one to be deleted
+          if (location == 0) {
+              top = top.next;
+              return true;
+          }
+          // Scenario 2: If the item to be deleted is in between the list or at the last
+          else {
+              MyNode myCurrentTempTop = top;
+              MyNode myPreviousTempTop = null;
+              int index = 0;
+              while (index < location) {
+                  myPreviousTempTop = myCurrentTempTop;
+                  myCurrentTempTop = myCurrentTempTop.next;
+                  index = index + 1;
+              }
+              myPreviousTempTop.next = myCurrentTempTop.next;  // Corrected line
+              return true;
+          }
       }
       return false;
-   }
-   
-   public String toString()
-   {
+  }
+
+  // bool Add(Object o)
+  public boolean Add(Object o) {
+      MyNode newNode = new MyNode();
+      newNode.item = o;
+      if (isEmpty()) {
+          top = newNode;
+      } else {
+          MyNode myCurrentTempTop = top;
+          while (myCurrentTempTop.next != null) {
+              myCurrentTempTop = myCurrentTempTop.next;
+          }
+          myCurrentTempTop.next = newNode;
+      }
+      return true;
+  }
+
+  // Object Get(int location)
+  public Object Get(int location) {
+      if (!isEmpty() && isLocationValid(location)) {
+          MyNode myCurrentTempTop = top;
+          int index = 0;
+          while (index < location) {
+              myCurrentTempTop = myCurrentTempTop.next;
+              index = index + 1;
+          }
+          if (myCurrentTempTop != null) {
+              return myCurrentTempTop.item;
+          }
+      }
+      return null;
+  }
+
+  // bool Edit(Object oldItem, Object newItem)
+  public boolean Edit(Object oldItem, Object newItem) {
+      if (!isEmpty() && Search(oldItem) && !Search(newItem)) {
+          MyNode myCurrentTempTop = top;
+          while (myCurrentTempTop != null) {
+              if (myCurrentTempTop.item.toString().equalsIgnoreCase(oldItem.toString())) {
+                  myCurrentTempTop.item = newItem;
+                  return true;
+              }
+              myCurrentTempTop = myCurrentTempTop.next;
+          }
+      }
+      return false;
+  }
+
+  // String toString()
+  @Override
+  public String toString() {
       String result = "";
-      if(!isEmpty())
-      {
-         MyNode tempTop = top;
-         while(tempTop != null)
-         {
-            result += "[" + tempTop.item + "]->";
-            tempTop = tempTop.next;
-         }
+      if (!isEmpty()) {
+          MyNode myCurrentTempTop = top;
+          while (myCurrentTempTop != null) {
+              result += "[" + myCurrentTempTop.item.toString() + "] -> ";
+              myCurrentTempTop = myCurrentTempTop.next;
+          }
       }
       return result;
-   }
-   
-   //version:
-   //get by location
-   //public boolean edit(Object old, Object newo)
-   
-   public Object get(int location)
-   {
-      if(!isEmpty() && isLocationValid(location))
-      {
-         int index =0;
-         MyNode tempTop = top;
+  }
 
-         while(index < location){
-            tempTop = tempTop.next;
-            index = index + 1;
-         }
-         if(tempTop != null)
-         {
-            return tempTop.item;
-         }
-         
-      }
+  // Helpers
+  // bool isFull()
+  public boolean isFull() {
       return false;
-   }
-   
-   public boolean edit(Object old, Object newObj){
-      if(!isEmpty() && !search(newObj)){
-         MyNode tempTop = top;
-         while(tempTop != null && !tempTop.item.toString().equalsIgnoreCase(old.toString())){
-            tempTop = tempTop.next;
-         }
-         if(tempTop != null)
-         {
-            tempTop.item = newObj;
-            return true;
-         }
-         
+  }
+
+  // bool isLocationValid(int location)
+  public boolean isLocationValid(int location) {
+      return location >= 0 && location <= getCount() - 1;
+  }
+
+  // bool isEmpty()
+  public boolean isEmpty() {
+      return top == null;
+  }
+
+  // int getCount()
+  public int getCount() {
+      int count = 0;
+      if (!isEmpty()) {
+          MyNode tempTop = top;
+          while (tempTop != null) {
+              count = count + 1;
+              tempTop = tempTop.next;
+          }
       }
-      return false;
-   }
-   
-   
+      return count;
+  }
 }
---
+++ Java/Acitivities-folder/SinglyLinkedList/MySinglyLinkedList_Copy.java
@@ -0,0 +1,235 @@
+// public class MySinglyLinkedList 
+// {
+//    MyNode top;
+//    int count;
+   
+//    public MySinglyLinkedList()
+//    {
+//       top = null;
+//       count = 0;
+//    }
+   
+//    public boolean isEmpty()
+//    {
+//       return top == null;
+//    }
+   
+//    public boolean isFull()
+//    {
+//       return false;
+//    }
+   
+//    public int getCount()
+//    {
+//       int count = 0;
+//       if(!isEmpty())
+//       {
+//          MyNode tempTop = top;
+//          while(tempTop != null)
+//          {
+//             tempTop = tempTop.next;
+//             count = count + 1;
+//          }        
+//       }
+//       return count;
+//    }
+   
+//    public boolean isLocationValid(int location)
+//    {
+//       return location >= 0 && location <= getCount() - 1;
+//    }
+   
+   
+//    public boolean add(Object o)
+//    {
+//       MyNode newNode = new MyNode();
+//       newNode.item = o;
+      
+//       if(isEmpty())
+//       {
+//          top = newNode;
+//       }
+//       else 
+//       {
+//          MyNode tempTop = top;
+//          while(tempTop.next != null)
+//          {
+//             tempTop = tempTop.next;
+//          }
+//          tempTop.next = newNode;         
+//       }
+//       return true;
+      
+//    }
+   
+//    public boolean search(Object o)
+//    {
+//       if(!isEmpty())
+//       {
+//          MyNode tempTop = top;
+//          while(tempTop != null)
+//          {
+//             if(tempTop.item.toString().equalsIgnoreCase(o.toString()))
+//             {
+//                return true;
+//             }
+//             tempTop = tempTop.next;
+//          }
+//       }
+//       return false;
+//    }
+   
+//    public int searchPart2(Object o)
+//    {
+//       if(!isEmpty())
+//       {
+//          MyNode tempTop = top;
+//          int location = 0;
+//          while(tempTop != null)
+//          {
+//             if(tempTop.item.toString().equalsIgnoreCase(o.toString()))
+//             {
+//                return location;
+//             }
+//             location = location + 1;
+//             tempTop = tempTop.next;            
+//          }
+//       }
+//       return -1;
+//    }
+   
+   
+//    public boolean delete(Object o)
+//    {
+//       if(!isEmpty() && search(o))
+//       {
+//          //scenario 1: the list contains only 1 node, that is the top
+//          //and that top is the node to be deleted
+//          if(top.item.toString().equalsIgnoreCase(o.toString()) && top.next == null)
+//          {
+//             top = null;
+//             return true;
+//          }
+//          //scenario 2: the list contains more than 1 nodes, and the node to be
+//          //deleted is the top
+//          else if(top.item.toString().equalsIgnoreCase(o.toString()) && top.next != null)
+//          {
+//             top = top.next;
+//             return true;
+//          }
+//          else 
+//          {
+//             System.out.println("*");
+//             //scenario 3: the list contains more that 1 nodes, and the node to be
+//             //deleted is somewhere in the middle or the last node
+//             MyNode tempCurrentTop = top;
+//             MyNode tempPreviousTop = null;
+            
+//             while(tempCurrentTop != null)
+//             {
+//                if(tempCurrentTop.item.toString().equalsIgnoreCase(o.toString()))
+//                {
+//                   break;
+//                }
+//                tempPreviousTop = tempCurrentTop;
+//                tempCurrentTop = tempCurrentTop.next;
+//             }
+            
+//             if(tempCurrentTop != null)
+//             {
+//                tempPreviousTop.next = tempCurrentTop.next;               
+//                return true;
+//             }                        
+//          }
+//       }
+//       return false;
+//    }
+   
+//    public boolean deleteByLocation(int location)
+//    {
+//       if(!isEmpty() && isLocationValid(location))
+//       {
+//          //scenario 1: if the location is 0, it means that it is the top
+//          //node, therefore, set the top to its top next
+//          if(location == 0)
+//          {
+//             top = top.next;
+//             return true;
+//          }
+//          //scenario 2: if the node to be deleted is in the middle of the list
+//          else 
+//          {
+//             int index = 0;
+//             MyNode tempCurrentTop = top;
+//             MyNode tempPreviousTop = null;
+//             while(index < location)
+//             {
+//                tempPreviousTop = tempCurrentTop;
+//                tempCurrentTop = tempCurrentTop.next;
+//                index = index + 1;
+//             }
+            
+//             tempPreviousTop.next = tempCurrentTop.next;
+//             return true;
+//          }
+//       }
+//       return false;
+//    }
+   
+//    public String toString()
+//    {
+//       String result = "";
+//       if(!isEmpty())
+//       {
+//          MyNode tempTop = top;
+//          while(tempTop != null)
+//          {
+//             result += "[" + tempTop.item + "]->";
+//             tempTop = tempTop.next;
+//          }
+//       }
+//       return result;
+//    }
+   
+//    //version:
+//    //get by location
+//    //public boolean edit(Object old, Object newo)
+   
+//    public Object get(int location)
+//    {
+//       if(!isEmpty() && isLocationValid(location))
+//       {
+//          int index =0;
+//          MyNode tempTop = top;
+
+//          while(index < location){
+//             tempTop = tempTop.next;
+//             index = index + 1;
+//          }
+//          if(tempTop != null)
+//          {
+//             return tempTop.item;
+//          }
+         
+//       }
+//       return false;
+//    }
+   
+//    public boolean edit(Object old, Object newObj){
+//       if(!isEmpty() && search(old) && !search(newObj)){
+//          MyNode tempTop = top;
+//          while(tempTop != null && !tempTop.item.toString().equalsIgnoreCase(old.toString())){
+//             tempTop = tempTop.next;
+//          }
+//          if(tempTop != null)
+//          {
+//             tempTop.item = newObj;
+//             return true;
+//          }
+         
+//       }
+//       return false;
+//    }
+   
+   
+// }
